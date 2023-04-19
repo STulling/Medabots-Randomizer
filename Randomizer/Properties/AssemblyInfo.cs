5bdf7f07b78dba4d53c1bcb4728aa28c989b2e69
YaibaToKen <kurokyoudai@live.com>
2023-04-19 11:52:35 +0100

    Corrected AssemblyInfo.cs

--- Randomizer/Properties/AssemblyInfo.cs
+++ Randomizer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Clean_Randomizer")]
+[assembly: AssemblyTitle("MedabotsRandomizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Clean_Randomizer")]
+[assembly: AssemblyProduct("MedabotsRandomizer")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
